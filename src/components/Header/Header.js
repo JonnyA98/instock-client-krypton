82238c62b29b54393a6f62e942ac21fb84ee10e6
jinsp43 <jinsp43@gmail.com>
2023-03-20 23:21:04 +0000

    add basic active link styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,19 @@
 import "./Header.scss";
 import logo from "../../assets/Logo/InStock-Logo_1x.png";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
     <header className="header">
       <img src={logo} alt="InStock Logo" className="header__logo" />
+      <div className="header__links">
+        <NavLink to="warehouses" className="header__link">
+          Warehouses
+        </NavLink>
+        <NavLink to="inventory" className="header__link">
+          Inventory
+        </NavLink>
+      </div>
     </header>
   );
 };
