363111e5f27830f310f785a144ea612e16bed99a
jinsp43 <jinsp43@gmail.com>
2023-03-22 14:08:12 +0000

    make header

--- src/pages/AddWarehouse/AddWarehouse.js
+++ src/pages/AddWarehouse/AddWarehouse.js
@@ -1,5 +1,15 @@
+import "./AddWarehouse.scss";
+import backBtn from "../../assets/Icons/arrow_back-24px.svg";
+
 const AddWarehouse = () => {
-  return;
+  return (
+    <main className="add-warehouse">
+      <div className="add-warehouse__heading-wrapper">
+        <img src={backBtn} alt="back button" />
+        <h1>Add New Warehouse</h1>
+      </div>
+    </main>
+  );
 };
 
 export default AddWarehouse;
