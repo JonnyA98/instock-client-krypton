c0e5376d709d3faab2e26d602f9a8fc3778b1ea9
Joe Gregson <joegregson@icloud.com>
2023-03-23 20:03:40 +0000

    refactor and flex for  responsive design warehouseCard

--- src/components/WarehouseCard/WarehouseCard.js
+++ src/components/WarehouseCard/WarehouseCard.js
@@ -1,43 +1,56 @@
 import { Link } from "react-router-dom";
 import deleteIcon from "../../assets/Icons/delete_outline-24px.svg";
 import editIcon from "../../assets/Icons/edit-24px.svg";
+import chevron from "../../assets/Icons/chevron_right-24px.svg";
 import "./WarehouseCard.scss";
 
 const WarehouseCard = ({ warehouse, modalToggle }) => {
   return (
     <article>
       <div className="warehouses__stock">
-        <div className="warehouses__facts">
-          <h2 className="warehouses__subtitle">WAREHOUSE</h2>
-          <Link
-            to={`/warehouses/${warehouse.id}`}
-            className="warehouses__description warehouses__link"
-          >
-            <p className="warehouses__info">{warehouse.warehouse_name}</p>
-          </Link>
+        <div className="warehouses__main-info-wrapper">
+          <div className="warehouses__name-add-wrapper">
+            <div className="warehouses__facts">
+              <h4 className="warehouses__subtitle">WAREHOUSE</h4>
+              <Link
+                to={`/warehouses/${warehouse.id}`}
+                className="warehouses__description warehouses__link-wrapper"
+              >
+                <h3 className="warehouses__info">{warehouse.warehouse_name}</h3>
+                <img
+                  src={chevron}
+                  alt=""
+                  className="warehouses__info-chevron"
+                />
+              </Link>
+            </div>
+            <div className="warehouses__facts">
+              <h4 className="warehouses__subtitle">ADDRESS</h4>
+              <p className="warehouses__info">{warehouse.address}</p>
+            </div>
+          </div>
+          <div className="warehouses__contact-wrapper">
+            <div className="warehouses__facts">
+              <h4 className="warehouses__subtitle">CONTACT NAME</h4>
+              <p className="warehouses__info">{warehouse.contact_name}</p>
+            </div>
+            <div className="warehouses__facts">
+              <h4 className="warehouses__subtitle">CONTACT INFORMATION</h4>
+              <p className="warehouses__info">{warehouse.contact_phone}</p>
+              <p className="warehouses__info">{warehouse.contact_email}</p>
+            </div>
+          </div>
         </div>
-        <div className="warehouses__facts">
-          <h2 className="warehouses__subtitle">CONTACT NAME</h2>
-          <p className="warehouses__info">{warehouse.contact_name}</p>
+        <div className="warehouses__action-wrapper">
+          <img
+            src={deleteIcon}
+            alt="delete Icon"
+            onClick={() => modalToggle(warehouse)}
+            className="warehouses__delete"
+          />
+
+          <img src={editIcon} alt="edit Icon" className="warehouses__edit" />
         </div>
-        <div className="warehouses__facts">
-          <h2 className="warehouses__subtitle">ADDRESS</h2>
-          <p className="warehouses__info">{warehouse.address}</p>
-        </div>
-        <div className="warehouses__facts">
-          <h2 className="warehouses__subtitle">CONTACT INFORMATION</h2>
-          <p className="warehouses__info">{warehouse.contact_phone}</p>
-          <p className="warehouses__info">{warehouse.contact_email}</p>
-        </div>
-        <span
-          onClick={() => modalToggle(warehouse)}
-          className="warehouses__delete"
-        >
-          <img src={deleteIcon} alt="delete Icon" />
-        </span>
-        <span className="warehouses__edit">
-          <img src={editIcon} alt="edit Icon" />
-        </span>
       </div>
     </article>
   );
