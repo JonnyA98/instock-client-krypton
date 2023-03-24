924246b550e2175c896d646306c1d522dd7545e8
alegna19 <angelagomezm@gmail.com>
2023-03-24 14:08:22 +0000

    change styles in InventoryCard

--- src/components/InventoryCard/InventoryCard.js
+++ src/components/InventoryCard/InventoryCard.js
@@ -1,74 +1,65 @@
 import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
-import "./InventoryCard.scss";
+//import "./InventoryCard.scss";
+import "../InventoryListItem/InventoryListItem.scss";
 import arrow from "../../assets/Icons/chevron_right-24px.svg";
 import bin from "../../assets/Icons/delete_outline-24px.svg";
 import pencil from "../../assets/Icons/edit-24px.svg";
 
 const InventoryCard = ({ inventories }) => {
   const [status, setStatus] = useState("In Stock");
-  const [statusClass, setStatusClass] = useState(
-    "warehouse-inventory-list-item__status"
-  );
+  const [statusClass, setStatusClass] = useState("inventory-list-item__status");
 
   useEffect(() => {
     if (inventories.quantity < 1) {
       setStatus("Out of Stock");
       setStatusClass(
-        "warehouse-inventory-list-item__status warehouse-inventory-list-item__status--out"
+        "inventory-list-item__status inventory-list-item__status--out"
       );
     }
   }, [inventories.quantity]);
 
   return (
     <>
-      <article className="warehouse-inventory-list-item">
-        <div className="warehouse-inventory-list-item__text">
-          <div className="warehouse-inventory-list-item__text-inner">
-            <div className="warehouse-inventory-list-item__category warehouse-inventory-list-item__category--link">
-              <h4 className="warehouse-inventory-list-item__mini-header">
+      <article className="inventory-list-item">
+        <div className="inventory-list-item__text">
+          <div className="inventory-list-item__text-inner">
+            <div className="inventory-list-item__category warehouse-inventory-list-item__category--link">
+              <h4 className="inventory-list-item__mini-header">
                 INVENTORY ITEM
               </h4>
-              <Link className="warehouse-inventory-list-item__link">
-                <p className="warehouse-inventory-list-item__link-text">
+              <Link className="inventory-list-item__link">
+                <p className="inventory-list-item__link-text">
                   {inventories.item_name}
                 </p>
                 <img src={arrow} alt="chevron_right" />
               </Link>
             </div>
-            <div className="warehouse-inventory-list-item__category">
-              <h4 className="warehouse-inventory-list-item__mini-header">
-                CATEGORY
-              </h4>
-              <p className="warehouse-inventory-list-item__data">
+            <div className="inventory-list-item__category">
+              <h4 className="inventory-list-item__mini-header">CATEGORY</h4>
+              <p className="inventory-list-item__data">
                 {inventories.category}
               </p>
             </div>
           </div>
-          <div className="warehouse-inventory-list-item__text-inner">
-            <div className="warehouse-inventory-list-item__category warehouse-inventory-list-item__category--status">
-              <h4 className="warehouse-inventory-list-item__mini-header">
-                STATUS
-              </h4>
+          <div className="inventory-list-item__text-inner">
+            <div className="inventory-list-item__category warehouse-inventory-list-item__category--status">
+              <h4 className="inventory-list-item__mini-header">STATUS</h4>
               <p className={statusClass}>{status}</p>
             </div>
-            <div className="warehouse-inventory-list-item__category">
-              <h4 className="warehouse-inventory-list-item__mini-header">
-                QTY
-              </h4>
-              <p className="warehouse-inventory-list-item__data">
+            <div className="inventory-list-item__category">
+              <h4 className="inventory-list-item__mini-header">QTY</h4>
+              <p className="inventory-list-item__data">
                 {inventories.quantity}
               </p>
-              <h4 className="warehouse-inventory-list-item__mini-header warehouse-inventory-list-item__warehouse-name ">
-                WAREHOUSE
-              </h4>
-              <p className="warehouse-inventory-list-item__data warehouse-inventory-list-item__description">
+              <h4 className="inventory-list-item__mini-header">WAREHOUSE</h4>
+              <p className="inventory-list-item__data inventory-list-item__warehouse-name ">
                 {inventories.warehouse_name}
               </p>
             </div>
           </div>
         </div>
-        <div className="warehouse-inventory-list-item__buttons">
+        <div className="inventory-list-item__buttons">
           <Link>
             <img src={bin} alt="bin" />
           </Link>
